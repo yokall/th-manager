3ecb7903b7b4f2de7e5fb371cf41467720b16c77
Colin Campbell <colincampbell321123@hotmail.com>
2016-10-05 22:25:10 +0100

    Get dummy list of cars from file and display in table

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,36 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
+import { Car } from './car';
+import { CarService } from './car.service';
 
 @Component({
+    moduleId: module.id,
     selector: 'my-app',
-    template: '<h1>Treasue hunt Manager</h1>'
+    templateUrl: 'app.component.html',
+    providers: [CarService]
 })
-export class AppComponent { }
+export class AppComponent {
+    title = 'Treasure Hunt Manager';
+    cars: Car[];
+    selectedCar: Car;
+    newCar: Car = new Car();
+
+    constructor(private carService: CarService) { }
+
+    ngOnInit(): void {
+        this.getCars();
+    }
+
+    getCars(): void {
+        this.carService.getCars().then(cars => this.cars = cars);
+    }
+
+    onSelect(car: Car): void {
+        this.selectedCar = car;
+    }
+
+    addCar(): void {
+        this.cars.push(this.newCar);
+        this.newCar = <Car>{};
+    }
+}
---
+++ app/mock-cars.ts
@@ -0,0 +1,14 @@
+import { Car } from './car';
+
+export const CARS: Car[] = [
+    { reg: 'CGZ 2149', name: 'Black Boomerang Surgeons' },
+    { reg: 'DGZ 2451', name: 'Scalding Orange Porcupines' },
+    { reg: '790 GXC', name: 'Brass Grunting Hatters' },
+    { reg: 'UOH 919', name: 'The Drug Mimes' },
+    { reg: 'KUY 629', name: 'Running Puck Wigglers' },
+    { reg: '160 BAU', name: 'The Ocean Groundhogs' },
+    { reg: 'KCE 445', name: 'Majestic InnerCity Muzak' },
+    { reg: '8 GOW', name: 'Cold Stoners' },
+    { reg: '2 TPO', name: 'The Cacophony' },
+    { reg: 'JNP 6', name: 'Grunting Gamers' }
+];
