b27ee54512abac9bcd49f82fd2b984c2d873b02b
Colin Campbell <colincampbell321123@hotmail.com>
2018-05-10 21:42:02 +0100

    Add cors support

--- server/index.js
+++ server/index.js
@@ -16,10 +16,16 @@ if (process.env.NODE_ENV == 'production') {
 mongoose.connect(db_conn_string);
 require('./models/cars');
 
+const app = express();
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 // Get our API routes
 const api = require('./routes/api');
-
-const app = express();
 
 // Gzip
 app.use(compression());
